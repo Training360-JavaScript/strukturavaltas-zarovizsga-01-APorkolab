a872129fa5845416ad757fec7ceda818ac15e327
Porkoláb Ádám <adam.porkolab@gmail.com>
2022-02-28 14:45:09 +0100

    First 2 tasks.

--- 1-DOM/dom.js
+++ 1-DOM/dom.js
@@ -1,5 +1,11 @@
 function manipulateDom() {
-    // 
+    document.querySelectorAll('.quote').forEach(item => {
+        item.style.borderColor = 'blue';
+        item.style.backgroundColor = 'lightblue';
+        item.style.fontStyle = 'italic';
+    });
 };
 
-export { manipulateDom };
+export {
+    manipulateDom
+};
